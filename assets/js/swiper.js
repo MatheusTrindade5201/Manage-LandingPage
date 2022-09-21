dbec9e4bae1896a4333f0616039cdfd984f072ea
Matheus Trindade <104238483+MatheusTrindade5201@users.noreply.github.com>
2022-09-21 20:08:28 -0300

    Inclusão do efeito de carrossel e do Pagination para o layout Mobile

---
+++ assets/js/swiper.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,  
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+   },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
