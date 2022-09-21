b22762e01f4a0f63ed726df1a0f06e0790be73de
Matheus Trindade <104238483+MatheusTrindade5201@users.noreply.github.com>
2022-09-21 20:42:16 -0300

    Responsivo para a quantidade de paginations bullets na seção de depoimentos

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,12 +1,24 @@
-const swiper = new Swiper('.swiper', {
-    slidesPerView: 1,  
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-   },
+function tamanhoTela() {
+    var telaLargura = window.screen.width
+    if(telaLargura <= 930){
+        var slides = 1
+    }else{
+        slides = 3}
 
-    // And if we need scrollbar
-    scrollbar: {
-        el: '.swiper-scrollbar',
-    },
-});
+    var swiper = new Swiper('.swiper', {
+        slidesPerView: slides,  
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+       },
+    
+        // And if we need scrollbar
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+    });
+}
+
+tamanhoTela();
+
+window.addEventListener('resize', tamanhoTela);
